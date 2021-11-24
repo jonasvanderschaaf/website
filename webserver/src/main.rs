0afd06959ec0558634705c68e9b9ca644ceccfe2
Jonas van der Schaaf <niet@jonasvanderschaaf.nl>
2021-11-24 22:48:45 +0100

    Initial commit

---
+++ webserver/src/main.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn hello_world() -> &'static str {
+    "Hello World!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello_world])
+}
