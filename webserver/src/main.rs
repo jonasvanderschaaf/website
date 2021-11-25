c5678480c0c74f24e3d8f48bc367d04f523e0772
Jonas van der Schaaf <niet@jonasvanderschaaf.nl>
2021-11-25 13:32:25 +0100

    Add Postgres database

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,12 +1,34 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::State;
+
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+
 #[get("/")]
 fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/database")]
+async fn test_database(pool: &State<Pool<Postgres>>) -> &'static str {
+    pool.acquire().await.unwrap();
+
+    "Success"
+}
+
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello_world])
+async fn rocket() -> _ {
+    let database_url = "postgresql://postgres@postgres:5432/database";
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url)
+        .await
+        .unwrap();
+
+    rocket::build()
+        .manage(pool)
+        .mount("/", routes![hello_world, test_database])
 }
